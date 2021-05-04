f518e2110da73799d974dbe4ec0f4cba8b3c4e63
Khadijah <khadijahlawal99@gmail.com>
2021-05-04 17:44:52 +0400

    added block hashing

---
+++ Sha256/Blocks/app.js
@@ -0,0 +1,12 @@
+//Import the Block class
+const BlockClass = require('./blocks.js');
+
+// Creating a block object
+const block = new BlockClass.Block("Test Block");
+
+// Generating the block hash
+block.generateHash().then((result) => {
+    console.log(`Block Hash: ${result.hash}`);
+    console.log(`Block: ${JSON.stringify(result)}`);
+}).catch((error) => {console.log(error)});
+
---
+++ Sha256/Blocks/blocks.js
@@ -0,0 +1,28 @@
+const SHA256 = require("crypto-js/sha256");
+class Block {
+  constructor(data) {
+    this.id = 0;
+    this.nonce = 144444;
+    this.body = data;
+    this.hash = "";
+  }
+
+  generateHash() {
+    // Use this to create a temporary reference of the class object
+    let self = this;
+    //Implement your code here
+    const hash = SHA256(JSON.stringify(self.data));
+    var promise = new Promise((resolve, reject) => {
+        if (hash) {
+            resolve({ hash: hash });
+        } else {
+            reject(new Error('fuck'));
+        }
+    });
+
+    return promise;
+  }
+}
+
+// Exporting the class Block to be reuse in other files
+module.exports.Block = Block;
